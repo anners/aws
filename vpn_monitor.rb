96bc80e196403a4ba599bd1a3c45157cd93b4c6d
Ann Wallace <ann.wallace@nike.com>
2015-09-02 18:38:52 -0700

    playing with a vpn monitor

---
+++ vpn_monitor.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# vpn monitor
+# requires ruby 2.1.X
+# TODO 
+
+require 'rubygems'
+require 'aws-sdk'
+#require 'log4r'
+require 'pp'
+
+regions = ["us-west-2", "ap-southeast-1", "ap-northeast-1"]
+regions.each do |region|
+	ec2 = Aws::EC2::Client.new(region: region)
+	#resp = ec2.describe_vpn_connections(vpn_connection_ids: ["vpn-XXXX"])'
+	resp = ec2.describe_vpn_connections()
+	resp.each do |vpns|
+		vpns.vpn_connections.each do |vpn|
+			if vpn.state.eql?("available")
+				tunnels_up = 0
+				vpn.vgw_telemetry.each do |tunnel|
+					if tunnel.status.eql?("UP")
+						tunnels_up += 1
+					end
+				end
+				puts "In #{region} #{vpn.vpn_connection_id} has #{tunnels_up} tunnel(s) up"
+			end
+		end
+	end
+end
+#pp resp
